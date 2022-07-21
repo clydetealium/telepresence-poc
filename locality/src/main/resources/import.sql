15fa414f8e017696cd064a37d5ef32d3e9acccfe
Clyde Tedrick <clyde.tedrick@tealium.com>
2022-07-21 02:30:31 -0400

    location changes

--- locality/src/main/resources/import.sql
+++ locality/src/main/resources/import.sql
@@ -1,4 +1,4 @@
 INSERT INTO locality(id, name, temperature) VALUES (1, 'San Diego', 78);
-INSERT INTO locality(id, name, temperature) VALUES (2, 'Chennai', 93);
-INSERT INTO locality(id, name, temperature) VALUES (3, 'Shiraz', 103);
+INSERT INTO locality(id, name, temperature) VALUES (2, 'Mumbai', 84);
+INSERT INTO locality(id, name, temperature) VALUES (3, 'Tehran', 102);
 INSERT INTO locality(id, name, temperature) VALUES (4, 'Rockville', 94);
