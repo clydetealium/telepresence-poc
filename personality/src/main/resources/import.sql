20b5e5bea9d7e38a0b824c02a48dd18fa3adba1f
Clyde Tedrick <clyde.tedrick@tealium.com>
2022-07-21 02:35:57 -0400

    personality persistence

---
+++ personality/src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO personality(id, name, threshold, locality) VALUES (1, 'Junior Seau', 95, 'San Diego');
+INSERT INTO personality(id, name, threshold, locality) VALUES (2, 'Sachin Tendulkar', 100, 'Mumbai');
+INSERT INTO personality(id, name, threshold, locality) VALUES (3, 'Maz Jobrani', 90, 'Tehran');
+INSERT INTO personality(id, name, threshold, locality) VALUES (4, 'Bravo', 90, 'Rockville');
