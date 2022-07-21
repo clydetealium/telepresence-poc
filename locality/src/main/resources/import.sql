bcc336d660dbb74da7f6d813a6fe7c03f4270772
Clyde Tedrick <clyde.tedrick@tealium.com>
2022-07-21 01:57:26 -0400

    SDLC-2445: locality persistence

---
+++ locality/src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO locality(id, name, temperature) VALUES (1, 'San Diego', 78);
+INSERT INTO locality(id, name, temperature) VALUES (2, 'Chennai', 93);
+INSERT INTO locality(id, name, temperature) VALUES (3, 'Shiraz', 103);
+INSERT INTO locality(id, name, temperature) VALUES (4, 'Rockville', 94);
